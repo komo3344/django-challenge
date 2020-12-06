c0d7b63dd730e2ea54954b5b6b88713d663aa28a
junyoung <jy940608@naver.com>
2020-12-06 19:19:18 +0900

    teset

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = [".elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["django-challenge-dev2.ap-northeast-2.elasticbeanstalk.com"]
 
 
 # Application definition
